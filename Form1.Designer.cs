324d26cda339c311bfbf124922ba4cf2669e25ff
hphuccntt <phuc54212420014@bachkhoasaigon.edu.vn>
2025-02-17 16:33:11 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Bai10_BangCuuC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSo = new System.Windows.Forms.TextBox();
+            this.btnTinh = new System.Windows.Forms.Button();
+            this.lsbTinh = new System.Windows.Forms.ListBox();
+            this.btnKQ = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bảng Cửu Chương";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "nhập";
+            // 
+            // txtSo
+            // 
+            this.txtSo.Location = new System.Drawing.Point(68, 56);
+            this.txtSo.Name = "txtSo";
+            this.txtSo.Size = new System.Drawing.Size(100, 20);
+            this.txtSo.TabIndex = 2;
+            // 
+            // btnTinh
+            // 
+            this.btnTinh.Location = new System.Drawing.Point(203, 54);
+            this.btnTinh.Name = "btnTinh";
+            this.btnTinh.Size = new System.Drawing.Size(75, 23);
+            this.btnTinh.TabIndex = 3;
+            this.btnTinh.Text = "KQ";
+            this.btnTinh.UseVisualStyleBackColor = true;
+            this.btnTinh.Click += new System.EventHandler(this.btnTinh_Click);
+            // 
+            // lsbTinh
+            // 
+            this.lsbTinh.FormattingEnabled = true;
+            this.lsbTinh.Location = new System.Drawing.Point(32, 108);
+            this.lsbTinh.Name = "lsbTinh";
+            this.lsbTinh.Size = new System.Drawing.Size(171, 108);
+            this.lsbTinh.TabIndex = 4;
+            // 
+            // btnKQ
+            // 
+            this.btnKQ.Location = new System.Drawing.Point(317, 54);
+            this.btnKQ.Name = "btnKQ";
+            this.btnKQ.Size = new System.Drawing.Size(75, 23);
+            this.btnKQ.TabIndex = 5;
+            this.btnKQ.Text = "KQM";
+            this.btnKQ.UseVisualStyleBackColor = true;
+            this.btnKQ.Click += new System.EventHandler(this.btnKQ_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKQ);
+            this.Controls.Add(this.lsbTinh);
+            this.Controls.Add(this.btnTinh);
+            this.Controls.Add(this.txtSo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSo;
+        private System.Windows.Forms.Button btnTinh;
+        private System.Windows.Forms.ListBox lsbTinh;
+        private System.Windows.Forms.Button btnKQ;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai10_BangCuuC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            lsbTinh.Items.Clear();
+            int so = Convert.ToInt32(txtSo.Text);
+            for (int i = 1; i <= 10; i++)
+            {
+                lsbTinh.Items.Add(so + " * " + i + " = " + so * i);
+            }
+        }
+
+        private void btnKQ_Click(object sender, EventArgs e)
+        {
+            
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                lsbTinh.Items.Add(i + " * " + j + " = " + i * j);
+            }
+        }
+    }
+}
